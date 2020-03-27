823322582045afab168af6ed16e61b35b975670a
Kat Maldon <katharinamaldon@gmail.com>
2020-03-27 20:01:17 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,7 @@
 def reverse_each_word(str)
   arr = str.split
   new_arr = []
-  arr.each do |word|
-    new_arr << word.reverse
+  arr.select { |word|new_arr << word.reverse
   end
   new_arr.join(" ")
 end
