7264365b3cde72ece36fa8e53be132286573a5e9
Kat Maldon <katharinamaldon@gmail.com>
2020-03-27 20:04:46 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(str)
   puts str.split().collect()
-  reversed = string.split().collect do |word|
+  reversed = str.split().collect do |word|
     word.reverse 
   end 
   reversed.join(" ")
